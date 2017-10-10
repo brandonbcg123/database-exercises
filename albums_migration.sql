1d2173b65d30ec741e6756422e32be4f2a157710
brandongossen <brandongossen@yahoo.com>
2017-10-10 11:13:28 -0500

    albums_migration.sql 2nd commit
    -Create a table called albums that has a column for id, artist, name of album, release date of album, amount of sales of record in millions, genre.
    
    -Set id as primary key

---
+++ albums_migration.sql
@@ -0,0 +1,20 @@
+USE codeup_test_db;
+
+DROP TABLE IF EXISTS albums;
+
+CREATE TABLE albums (
+  id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+
+  artist VARCHAR(150) NOT NULL,
+
+  name  VARCHAR(150) NOT NULL,
+
+  release_date DATE NOT NULL,
+
+  sales FLOAT(3,2) NOT NULL,
+
+  genre VARCHAR(50) NOT NULL,
+
+  PRIMARY KEY (id)
+);
+
