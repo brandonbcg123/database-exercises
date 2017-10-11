ba8b51b5d6cc93526be7bd9b85c8b2e328550f75
brandongossen <brandongossen@yahoo.com>
2017-10-11 15:17:52 -0500

    Delete Exercise 3rd Commit
    -Add SELECT statements to add captions before each query

--- delete_exercises.sql
+++ delete_exercises.sql
@@ -1,13 +1,16 @@
 USE codeup_test_db;
 
+SELECT 'Below are the albums in the table released after 1991' AS '';
 SELECT name AS 'Album Name:', release_date AS 'Release Date'
 FROM albums
 WHERE release_date > 1991;
 
+SELECT 'Below are the albums in the table with the genre "disco"' AS '';
 SELECT name AS 'Album Name:', genre AS 'Genre'
 FROM albums
 WHERE genre = 'disco';
 
+SELECT 'Below are the albums in the table by Whitney Houston' AS '';
 SELECT name AS 'Album Name:', artist AS 'Artist:'
 FROM albums
 WHERE artist = 'Whitney Houston';
