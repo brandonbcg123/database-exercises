97c02adb3692dec853e5bc39589bb9fd0c6d7cb1
brandongossen <brandongossen@yahoo.com>
2017-10-11 14:04:23 -0500

    sql update_exercises.sql Final Commit
    -Add an UPDATE statement to albums and use the SET keyword to increase all albums sales by 10 times their original amount.
    
    -Add an UPDATE statement to albums and use the SET keyword to update all albums before 1980 back to the 1800s by subtracting the release_date by 100.
    
    -Add an UPDATE statement to albums and use the SET keyword to change any artist with the name Michael Jackson to Peter Jackson.

--- update_exercises.sql
+++ update_exercises.sql
@@ -3,17 +3,48 @@ USE codeup_test_db;
 SELECT *
 FROM albums;
 
+SELECT '------------------------------------------------------';
+
+SELECT artist AS 'Artist:', name AS 'Album:', sales AS 'Sales:'
+FROM albums;
+
+SELECT '------------------------------------------------------' AS '';
+
+UPDATE albums
+SET sales = (sales * 10);
+
+SELECT id, artist AS 'Artist:', name AS 'Album:', sales AS 'Sales:'
+FROM albums;
+
+SELECT '------------------------------------------------------';
+
 SELECT id, artist AS 'Artist:', name AS 'Album:', release_date AS 'Release Date:'
 FROM albums
 WHERE release_date < 1980;
+
+SELECT '------------------------------------------------------' AS '';
+
+UPDATE albums
+SET release_date = (release_date - 100)
+WHERE release_date < 1980;
+
+
+SELECT id, artist AS 'Artist:', name AS 'Album:', release_date AS 'Release Date:'
+FROM albums
+WHERE release_date < 1980;
+
+SELECT '------------------------------------------------------';
 
 SELECT id, artist AS 'Artist:', name AS 'Album:'
 FROM albums
 WHERE artist = 'Michael Jackson';
 
--- UPDATE albums
--- SET
--- WHERE;
+SELECT '------------------------------------------------------' AS '';
 
--- SELECT *
--- FROM albums;
+UPDATE albums
+SET artist = 'Peter Jackson'
+WHERE artist = 'Michael Jackson';
+
+SELECT id, artist AS 'Artist:', name AS 'Album:'
+FROM albums
+WHERE artist = 'Peter Jackson';
