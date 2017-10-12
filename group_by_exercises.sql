a62b34ff6e51c4410b7d51d65b6628f624ebcbe8
brandongossen <brandongossen@yahoo.com>
2017-10-12 11:09:09 -0500

    SQL group_by_exercises.sql 2nd commit
    -Update the query that finds the unique titles in the titles table to sort the results alphabetically by using the GROUP BY clause.
    
    -Update the query for employees whose last names start and end with 'E' to only find the unique last names (Where there are no duplicate last names) that start and end with 'E' using the GROUP BY clause.

--- group_by_exercises.sql
+++ group_by_exercises.sql
@@ -1,4 +1,15 @@
 USE employees;
 
-SELECT DISTINCT title
-FROM titles;
+-- SELECT DISTINCT title
+-- FROM titles;
+
+SELECT title
+FROM titles
+GROUP BY title ASC;
+
+
+SELECT last_name
+FROM employees
+WHERE last_name LIKE 'E%'
+      AND last_name LIKE '%E'
+GROUP BY last_name;
