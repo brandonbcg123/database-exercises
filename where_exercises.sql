7642d369f67cb3c5eda3a6a334d754c8451f8c07
brandongossen <brandongossen@yahoo.com>
2017-10-11 22:41:25 -0500

    SQL where_exercises.sql Final commit
    -Update the query with first names 'Irena', 'Vidya', or 'Maya' by replacing the IN keyword inside of the WHERE clause with an "OR" keyword for each name.
    
    -Add a condition for the above query where only males with first names 'Irena', 'Vidya', or 'Maya' are selected through chaining the gender property with a value of 'M' to those names. This is done by placing the gender property right after the WHERE clause and then using the "AND" keyword followed by () in which the name conditions will be inputted into so all 3 names are chained with the value of 'M'.
    
    -Update the query with last names that start with 'E' to last names that either start or end with 'E' by adding the "OR" Keyword after the first condition (LIKE 'E%') in which the second condition will contain the keyword LIKE with a wildcard before the 'E'.
    
    -Dupicate the query above with last names that start or end with 'E' and change the conditions where the last names selected have to both start and end with an 'E' by changing the "OR" Keyword to an "AND".
    
    -Combine the query that find employees that were born on Christmas with the query that find employees hired in the 1990s by connecting the conditions of the two queries with the keyword "AND".
    
    -Update the query that find employees with a 'q' in their last name to not include employees with a 'qu' in their last name by adding the "AND" keyword after the original condition (LIKE '%q%') in which the second condition will contain the keywords NOT LIKE with a wildcard before and after the 'qu'.

--- where_exercises.sql
+++ where_exercises.sql
@@ -1,21 +1,57 @@
 USE employees;
 
-SELECT *
-FROM employees
-WHERE first_name IN ('Irena', 'Vidya', 'Maya');
+-- Chaining WHERE Clause Versions
 
 SELECT *
 FROM employees
-WHERE last_name LIKE 'E%';
+WHERE gender = 'M'
+      AND (
+        first_name = 'Irena'
+        OR first_name = 'Vidya'
+        OR first_name = 'Maya'
+      );
 
 SELECT *
 FROM employees
-WHERE hire_date BETWEEN '1990-01-01' AND '1999-12-31';
+WHERE last_name LIKE 'E%'
+      OR last_name LIKE '%E';
 
 SELECT *
 FROM employees
-WHERE birth_date LIKE '%-12-25';
+WHERE last_name LIKE 'E%'
+      AND last_name LIKE '%E';
 
 SELECT *
 FROM employees
-WHERE last_name LIKE '%q%';
+WHERE hire_date BETWEEN '1990-01-01' AND '1999-12-31'
+      AND birth_date LIKE '%-12-25';
+
+SELECT *
+FROM employees
+WHERE last_name LIKE '%q%'
+      AND last_name NOT LIKE '%qu%';
+
+-- Old Version
+# SELECT *
+# FROM employees
+# WHERE first_name IN ('Irena', 'Vidya', 'Maya');
+
+-- Old Version
+# SELECT *
+# FROM employees
+# WHERE last_name LIKE 'E%';
+
+-- Old Version
+# SELECT *
+# FROM employees
+# WHERE hire_date BETWEEN '1990-01-01' AND '1999-12-31';
+
+-- Old Version
+# SELECT *
+# FROM employees
+# WHERE birth_date LIKE '%-12-25';
+
+-- Old Version
+# SELECT *
+# FROM employees
+# WHERE last_name LIKE '%q%';
