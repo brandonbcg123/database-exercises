ecd7c8fe43ee4e0248ab6fc1f7ae24dccbb7c782
brandongossen <brandongossen@yahoo.com>
2017-10-12 14:24:34 -0500

    SQL limit_exercises.sql 2nd commit
    -Input the query employees born on Christmas and hired in the 90s from order_by_exercises.sql into limit_exercises.sql
    
    -Update the query employees born on Christmas and hired in the 90s to find just the first 5 employees
    by having the SELECT statement include first_name, last_name, birth_date, and hire_date, adding a GROUP BY clause above the ORDER BY clause that includes the same properties as the SELECT Statement, and adding a LIMIT clause at the end of the query.

--- limit_exercises.sql
+++ limit_exercises.sql
@@ -4,3 +4,11 @@ SELECT DISTINCT last_name
 FROM employees
 GROUP BY last_name DESC
 LIMIT 10;
+
+SELECT first_name, last_name, birth_date, hire_date
+FROM employees
+WHERE hire_date BETWEEN '1990-01-01' AND '1999-12-31'
+      AND birth_date LIKE '%-12-25'
+GROUP BY first_name, last_name, birth_date, hire_date
+ORDER BY birth_date ASC, hire_date DESC
+LIMIT 5;
