3446589013dfb725b4a6609e4f4020fda8c6138b
brandongossen <brandongossen@yahoo.com>
2017-10-13 09:36:57 -0500

    SQL aliases_exercises.sql Final Commit
    -use the CONCAT method to combine the emp_no with last name and first name and set the full CONCAT as full_name.
    
    -Add the date of birth for each employee using the alias 'DOB' to the query by using the AS keyword in the SELECT statement.

--- aliases_exercises.sql
+++ aliases_exercises.sql
@@ -1,6 +1,7 @@
 USE employees;
 
-SELECT CONCAT(last_name, ', ', first_name) AS 'full_name'
+SELECT CONCAT(emp_no, ' - ', last_name, ', ', first_name) AS 'full_name', birth_date AS 'DOB'
 FROM employees
-GROUP BY full_name
+GROUP BY full_name ,emp_no
+ORDER BY emp_no
 LIMIT 10;
