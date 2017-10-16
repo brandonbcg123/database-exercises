c570cb15fe59f965eef9c3db09115dc5a560aaa7
brandongossen <brandongossen@yahoo.com>
2017-10-16 13:35:01 -0500

    SQL subqueries_exercises.sql 3rd and Final commit
    -Find all the current department managers that are female.

--- subqueries_exercises.sql
+++ subqueries_exercises.sql
@@ -1,5 +1,6 @@
 USE employees;
 
+-- All the employees with the same hire date as employee 101010 using a sub-query.
 SELECT emp_no, first_name, last_name, hire_date
 FROM employees
 WHERE hire_date IN (
@@ -26,3 +27,12 @@ WHERE emp_no IN (
 WHERE first_name = 'Aamod'
 )
 GROUP BY title;
+
+-- Current department managers that are female.
+SELECT first_name, last_name, gender, hire_date
+FROM employees
+WHERE emp_no IN (
+  SELECT emp_no
+  FROM dept_manager
+  WHERE to_date >= curdate()
+) AND gender = 'F';
