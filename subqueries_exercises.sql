b055ac5870645c5863cb2f4f7aa2aa6bffd02394
brandongossen <brandongossen@yahoo.com>
2017-10-16 13:24:54 -0500

    SQL subqueries_exercises.sql 2nd commit
    -Find all the titles held by all employees with the first name Aamod by making title form the table titles as the main query and the emp_no from the employees table as the subquery.
    
    -Create the same table that only shows the unique titles for that same employee by implementing a GROUP BY clause at the very end of the entire query.

--- subqueries_exercises.sql
+++ subqueries_exercises.sql
@@ -7,3 +7,22 @@ WHERE hire_date IN (
   FROM employees
   WHERE emp_no = 101010
 );
+
+-- Total Titles for employees whose first name is 'Aamod'
+SELECT title
+FROM titles
+WHERE emp_no IN (
+  SELECT emp_no
+  FROM employees
+  WHERE first_name = 'Aamod'
+);
+
+-- Unique Titles for employees whose first name is 'Aamod'
+SELECT title, count(title) AS 'Count'
+FROM titles
+WHERE emp_no IN (
+  SELECT emp_no
+    FROM employees
+WHERE first_name = 'Aamod'
+)
+GROUP BY title;
